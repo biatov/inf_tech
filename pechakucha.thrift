2165eea588c2fada00f0d1a7ad340e2e44887c80
Sergey Byatov <biatov.s@gmail.com>
2017-06-22 14:18:53 +0300

    new vision on pechakucha.thrift

--- pechakucha.thrift
+++ pechakucha.thrift
@@ -1,30 +1,34 @@
-namespace java thrift.example
-
-enum WatchType {
-    WATCH,
-    AUTHENTIFICATE = False,
-    APPLOUD = False
-}
-
-struct Pechakucha {
-    1: optional WatchType watchType = WatchType.WATCH;
-    2: optional WatchType authentificateType = WatchType.AUTHENTIFICATE;
-    4: optional WatchType ApploudType = WatchType.APPLOUD;
-    3: optional string language = "english";
-}
-
-
 namespace py pechakucha
 
-enum Action {
-    AUTHORISATION = 1,
-    APPLOUD = 2
+struct AboutPresenter {
+    1: required string authorName;
+    2: required string authorDescription;
 }
 
-struct Watch {
-    1:
+struct Location {
+    1: required double latitude;
+    2: required double longitude;
 }
 
-service CalcService{
-    CalcResult addition(1: i32 first, 2: i32 second)
+struct SearchAttributes {
+    1: required string categories;
+    2: required string tags;
+}
+
+struct WatchPresentation {
+    1: required i32 countWatches;
+    1: optional i32 countApplause;
+}
+
+struct Presentation {
+    1: required string presentationName;
+    2: required list<AboutPresenter> Presenter;
+    3: required TimeStamp dateTime;
+    4: required Location loc;
+    5: required list<SearchAttributes> Search;
+    6: required string presentationDescription;
+}
+
+service Post {
+    1: oneway publishPresentation(presentation: Presentation) : void
 }
